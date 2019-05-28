ed950d3aef47561e1c60b8a370ce55d787182836
brlumen <igflocal@gmail.com>
2019-05-28 17:30:43 +0700

    Bump version to 1.0.1

--- SearchRelatedIssue/SearchRelatedIssue.php
+++ SearchRelatedIssue/SearchRelatedIssue.php
@@ -21,7 +21,7 @@ class SearchRelatedIssuePlugin extends MantisPlugin {
         $this->name        = 'SearchRelatedIssue';
         $this->description = plugin_lang_get( 'description' );
 
-        $this->version  = '1.0.1-dev';
+        $this->version  = '1.0.1';
         $this->requires = array(
                                   'MantisCore' => '2.0.0',
         );
